5ae8b41395d5287723d50e33f6c2588bd846cbe6
KwameSei <nathanielosei92@gmail.com>
2023-05-23 04:23:49 +0000

    Added error handling

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,39 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url} ${new Date().toLocaleTimeString()}`);
+    next();
+});
+
 // Routes
 app.use('/api/users', userRoutes);
 
+// Error handling
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send('Something went wrong');
+});
+
+app.use((req, res, next) => {
+    res.status(404).send('Not found');
+});
+
+app.use((req, res, next) => {
+    res.status(403).send('Forbidden');
+});
+
+app.use((req, res, next) => {
+    res.status(401).send('Unauthorized');
+});
+
+app.use((req, res, next) => {
+    res.status(400).send('Bad request');
+});
+
+app.use((req, res, next) => {
+    res.status(204).send('No content');
+});
 
 app.listen(PORT, hostname, () => {
     console.log(`Server running at http://${hostname}:${PORT}/`);
