3d6b08ab29f52798f5c3ff53be94e89d5a773c90
KwameSei <nathanielosei92@gmail.com>
2023-05-27 06:46:18 +0000

    Server - mongodb setup created

--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,92 @@
-const express = require('express');
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import multer from 'multer';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import { fileURLToPath } from 'url';
 
-// Routes
-const userRoutes = require('./routes/user');
+dotenv.config();
 
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);  // Get the filename of the current module
+const __dirname = path.dirname(__filename);  // Get the directory name of the current module
+dotenv.config()
 const app = express();
-
-const hostname = "127.0.0.1";
-const PORT = process.env.PORT || 5000;
+const { MONGO_URL, PORT } = process.env;
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+// app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));    // parse application/x-www-form-urlencoded
+app.use(bodyParser.json({ limit: '30mb', extended: true }));    // parse application/json
+app.use(morgan('common'));  // HTTP request logger middleware for node.js
+app.use(cors());
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));  // Set the static folder
 
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url} ${new Date().toLocaleTimeString()}`);
-    next();
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/assets/uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
 });
+const upload = multer({ storage: storage });
 
-// Routes
-app.use('/api/users', userRoutes);
+/* DATABASE */
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => {
+      app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+    })
+    .catch((error) => {
+      console.error('Error connecting to MongoDB:', error.message);
+    });
 
-// Error handling
-app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).send('Something went wrong');
-});
+// app.use((req, res, next) => {
+//     console.log(`${req.method} ${req.url} ${new Date().toLocaleTimeString()}`);
+//     next();
+// });
 
-app.use((req, res, next) => {
-    res.status(404).send('Not found');
-});
+// // Routes
+// app.use('/api/users', userRoutes);
 
-app.use((req, res, next) => {
-    res.status(403).send('Forbidden');
-});
+// // Error handling
+// app.use((err, req, res, next) => {
+//     console.log(err.stack);
+//     // res.status(500).send('Something went wrong');
+//     res.sendFile(path.join(__dirname, '../../public/Errors/500.html'))
+// });
 
-app.use((req, res, next) => {
-    res.status(401).send('Unauthorized');
-});
+// app.use((req, res, next) => {
+//     res.status(404).send('Not found');
+// });
 
-app.use((req, res, next) => {
-    res.status(400).send('Bad request');
-});
+// app.use((req, res, next) => {
+//     res.status(403).send('Forbidden');
+// });
 
-app.use((req, res, next) => {
-    res.status(204).send('No content');
-});
+// app.use((req, res, next) => {
+//     res.status(401).send('Unauthorized');
+// });
 
-app.listen(PORT, hostname, () => {
-    console.log(`Server running at http://${hostname}:${PORT}/`);
-});
+// app.use((req, res, next) => {
+//     res.status(400).send('Bad request');
+// });
+
+// app.use((req, res, next) => {
+//     res.status(204).send('No content');
+// });
+
+// app.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${PORT}/`);
+// });
